ec00d33bfcdb755d70e99c2f4a30b5fa01e02061
Nicolas Andrews S <nm.andrewss@gmail.com>
2020-05-01 02:18:32 -0400

    test omp

--- main.cpp
+++ main.cpp
@@ -4,29 +4,43 @@
 #include <string>
 #include <chrono>
 
+#define DEBUG_MODE !true
+
 float calcMean(const std::vector<int>& v);
-std::vector<int> tokenize(const std::string& s,const char delim= ';');
+std::pair<int, float> tokenize(const std::string s,const char delim= ';');
 
 int main(int argc, char** argv) { 
     auto start = std::chrono::high_resolution_clock::now(); 
-    if(argv[1] == NULL) {
+    if(DEBUG_MODE && argv[1] == NULL) {
         std::cout << "ERROR: Archivo no seleccionado\n";
         return EXIT_FAILURE;
     }
-    std::ifstream file(argv[1]);
+
+    std::ifstream file("archivo.csv");
     std::cout << "i: " << argv[1] << std::endl;
     std::string outputStr = "promediosPSU.csv";
     std::cout << "o: " << outputStr << std::endl;
     std::ofstream output(outputStr);
-    std::vector<int> values;
+    std::pair<int,float> pair;
     std::string build="";
-    float mean = 0.f;
-    for(std::string line; std::getline(file, line);) {
-        values = tokenize(line);
-        mean = calcMean(values);
-        build = std::to_string(values[0]) + ";" + std::to_string(mean) + "\n";
+    int numlines=-1;
+    std::string line;
+        while(!file.eof()) {
+            std::getline(file,line);
+            numlines = numlines + 1;
+        }
+    file.close();
+    file.open(argv[1]);
+
+    #pragma omp parallel for ordered schedule(static)
+    for(int i = 0; i < numlines; i++) {
+        std::getline(file,line);
+        pair = tokenize(line);
+        build = std::to_string(pair.first) + ";" + std::to_string(pair.second) + "\n";
+    #pragma omp ordered
         output << build;
     }
+
     file.close();
     output.close();
     std::string pause;
@@ -41,23 +55,27 @@ int main(int argc, char** argv) {
 float calcMean(const std::vector<int>& v) {
     int r = 0;
     float f = 0.f;
-    for(int i = 1; i < 6; i++) {  //rut0, Nem1, ranking2, cs3, historias4, mat5, leng6 ; v[0] es el rut.
+    #pragma omp simd reduction(+:r)
+    for(unsigned int i = 1; i < v.size(); i++) {  //rut0, Nem1, ranking2, cs3, historias4, mat5, leng6 ; v[0] es el rut.
         r += v[i];
     }
     f = (float)r/6.0;
     return f;
 }
 
-std::vector<int> tokenize(const std::string& s,const char delim) {
-    std::vector<int> ret_val;
+std::pair<int,float> tokenize(const std::string s,const char delim) {
+    std::pair<int,float> ret_val;
+    std::vector<int> vec;
     std::string str;
     for(auto c : s) {
         if(c == delim) {
-            ret_val.push_back(std::stoi(str));
+            vec.push_back(std::stoi(str));
             str="";
         }else {
             str += c;
         }
     }
+    ret_val.first = vec[0];
+    ret_val.second = calcMean(vec);
     return ret_val;
 }
